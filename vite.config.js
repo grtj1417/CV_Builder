f6aec3c7e592b97fe9fed1d7839345a44c5e9387
grtj1417 <grtj1417@gmail.com>
2024-06-20 22:43:12 +0800

    edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [vue()],
+  base: '/CV_Builder/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
